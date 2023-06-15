3615456694ca04a733efce9a81f2c2f49c8bf28b
NtengaSthabile <ntengasthabile23@gmail.com>
2023-06-15 16:12:30 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,11 +2,11 @@
 /**
  * main - Entry point
  *
- * Return: Always 0 (Success)
+ * Return: Always0(Success)
  *
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
-	retun(0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return(0);
 }
