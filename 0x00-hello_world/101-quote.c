4685f4a13d6b9be1f14a88ec300d6bbc821bde90
NtengaSthabile <ntengasthabile23@gmail.com>
2023-06-15 18:49:48 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,13 @@
-#include<stdio.h>
-#include<unist.h>
+#include <stdio.h>
+#include <unist.h>
 
 /**
- * main - Entry point
+ * main - A c program that print a line to the starndard error
  *
  * Return: Always 1 (Success)
  */
 int main(void)
 {
-	write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",59);
+	fput(2,"and that piece of art is useful\" - Dora Korpar,2015-10-19\n",stdout);
 	return(1);
 }
