a168f7324bdb10f4a5d57b138bddd1203b267d51
NtengaSthabile <ntengasthabile23@gmail.com>
2023-06-20 11:51:04 -0700

    print

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <sdlib.h>
+#include <time.h>
+
+/**
+ * main - Print the alphabet.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char i;
+
+	for (i = 'A'; i < = 'Z'; i++)
+
+		putchar(i);
+	for (i = 'a'; i < = 'z'; i++)
+		putchar(i);
+		putchar('\n')
+	return (0);
+}
