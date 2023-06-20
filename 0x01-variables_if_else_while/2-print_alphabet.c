1b458eff0a030e0f14a79436a303de1838bcbdaf
NtengaSthabile <ntengasthabile23@gmail.com>
2023-06-20 09:10:08 -0700

    alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,19 +1,16 @@
 #include <stdio.h>
-
 /**
- * main - Print the alphabet.
- *
+ * main - Entnry point
  * Return: Always 0 (Success)
  */
-int main(void)
-{
-	char a(26) = "abcdefghijklmnopqrstuvwxyz",
-		int i;
-	for (i = 0; i < 26 ; i++); 
-	
-	{
-		putchar(alp(i));
-	}
-	putchar('\n');
 
-		return (0);
+int main(void)
+
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	putchar(c)
+	putchar('\n');
+	return (0);
+}	
