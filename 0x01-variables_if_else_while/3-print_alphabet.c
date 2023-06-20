c03e44ba361a4e311d7cc196ebdc1b5775e9be36
NtengaSthabile <ntengasthabile23@gmail.com>
2023-06-20 11:13:52 -0700

    3-print

---
+++ 0x01-variables_if_else_while/3-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stlib.h>
+#include <time.h>
+
+/**
+ * main - Print the alphabet in lowercase and uppercase
+ *
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char low;
+	for (low = 'a'; low <= 'z' ; low * *)
+		putchar(low);
+
+	for (low = 'A'; low <= 'z' ; low * *)
+		putchar(low);
+		putchar('\n');
+
+		return (0);
