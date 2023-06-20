836875dd982ed899393acc285439012798566900
NtengaSthabile <ntengasthabile23@gmail.com>
2023-06-20 08:15:10 -0700

    alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -7,11 +7,12 @@
  */
 int main(void)
 {
-	char alp[26]= "abcdefghijklmnopqrstuvwxyz",
+	char a(26) = "abcdefghijklmnopqrstuvwxyz",
 		int i;
-	for(i = 0; i < 26;i++)
+	for (i = 0; i < 26 ; i++); 
+	
 	{
-		putchar(alp[i]);
+		putchar(alp(i));
 	}
 	putchar('\n');
 
