3ab37d979f6a9b9225d070a8c05989530e23ab60
NtengaSthabile <ntengasthabile23@gmail.com>
2023-06-20 09:33:14 -0700

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main - Entnry point
+ * main - The output for this programm should be alphabet in lowercase
  * Return: Always 0 (Success)
  */
 
@@ -10,7 +10,7 @@ int main(void)
 	char c;
 
 	for (c = 'a'; c <= 'z'; c++)
-	putchar(c)
+	putchar(c);
 	putchar('\n');
 	return (0);
 }	
