53ba5843015a86552ec375397db03e091ebf11c3
NtengaSthabile <ntengasthabile23@gmail.com>
2023-06-23 11:53:40 -0700

    isupper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -6,11 +6,11 @@
  * Return: 1 for upper letter or 0 for any else
  */
 
-int_isupper(int x)
+int _isupper(int x)
 {
-	if (x >=65 && x <= 90)
+	if (x >= 65 && x <= 90)
 	{
 	return (1);
 	}
-	return(0);
+	return (0);
 }
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,12 +1,23 @@
 int _puthchar(char c);
+
 int _isupper(int c);
+
 int _isdigit(int c);
+
 int mul(int a,int c);
+
 void print_number(void);
+
 void print_most_numbers(void);
+
 void more_numbers(void);
+
 void print_line(int n);
+
 void print_daigonal(int n);
+
 void print_squre(int size);
+
 void print_traingle(int size);
+
 void print_number(int n);
