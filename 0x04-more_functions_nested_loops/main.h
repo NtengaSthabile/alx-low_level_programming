208dc24a83d77fc39f8b5a4e226f598c8ed3c6b9
NtengaSthabile <ntengasthabile23@gmail.com>
2023-06-23 11:44:11 -0700

    isupper

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,12 @@
+int _puthchar(char c);
+int _isupper(int c);
+int _isdigit(int c);
+int mul(int a,int c);
+void print_number(void);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int n);
+void print_daigonal(int n);
+void print_squre(int size);
+void print_traingle(int size);
+void print_number(int n);
