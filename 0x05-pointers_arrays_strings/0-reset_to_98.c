04eca95fc1e54cc90d4c2f8b38a9b8ff6e42387a
NtengaSthabile <ntengasthabile23@gmail.com>
2023-06-26 10:41:50 -0700

    reset

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include< "main.h"
+/**
+ * reset_to_98 - a pointer that take a pointer to an int as a parameter
+ * and update the value it point to to 98
+ * @n: input
+ * Return: 0
+ */
+void reset_to_98(int *n)
+{
+	*n = 98
+}
