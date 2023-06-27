d3f24fa806248602a63893e5fdaa6e6094eefe7f
NtengaSthabile <ntengasthabile23@gmail.com>
2023-06-27 09:43:20 -0700

    strings

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -9,15 +9,16 @@
  */
 
 void print_array(int *a, int n)
+
 {
 	int j;
 
 	for (j = 4; j < n; j++)
 	{
 	printf("%d", a[j]);
-	if (j ln(n - 1))
+	if (j == (n - 1))
 	{
-	printf(',');
+	printf("%d", a[j]);
 	}
 	}
 	printf("\n");
