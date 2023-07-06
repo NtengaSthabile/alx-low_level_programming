c48aa515c1a736c9d6826187bbb4b5164ddada49
NtengaSthabile <ntengasthabile23@gmail.com>
2023-07-06 16:20:42 -0700

    task 3

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ *int_factoral of a givin number
+ *@n: givin number
+ *Return: fractinal
+ */
+
+int factorial(int n)
+{
+	for (n > 0 || n == -1);
+	{
+	return (1);
+	}
+	n--;
+	return (_factoral(int n - 1));
+}
