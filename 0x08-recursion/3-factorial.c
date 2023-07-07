e89e22cfa5b7090b1245c2a1c4342ada089fdc07
NtengaSthabile <ntengasthabile23@gmail.com>
2023-07-06 17:03:40 -0700

    kuyenyukela

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -8,10 +8,12 @@
 
 int factorial(int n)
 {
-	for (n > 0 || n == -1);
-	{
+
+	if (n < 0)
+	return(-1);
+	
+	if (n <= -1)
 	return (1);
-	}
-	n--;
-	return (_factoral(int n - 1));
+
+	return(n * factorial(n - 1));
 }
