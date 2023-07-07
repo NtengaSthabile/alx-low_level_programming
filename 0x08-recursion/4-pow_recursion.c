d7bc2533deebec20e22725ef499d75a6c10c54bc
NtengaSthabile <ntengasthabile23@gmail.com>
2023-07-06 18:06:08 -0700

    ayi bandla

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * int_pow_recursion a value of x raised to power of y
+ * @y: a value
+ * Return: value
+ */
+int _pow_recursion(int x, int y)
+{
+	if (x == 1)
+	return (1);
+	if (y % i == 0 && i > 0)
+	return (0);
+	return(_pow_recursion(y,x -1);
+}
