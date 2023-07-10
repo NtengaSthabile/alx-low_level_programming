72835af1b426592c45eda03d8490274b90aefe4b
NtengaSthabile <ntengasthabile23@gmail.com>
2023-07-10 13:47:57 -0700

    fake love

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * create_arrays- creates an  array of chars
+ * @size: The sizeof the array
+ * @c: The char to fill in  the array
+ *
+ * Return: The array filled
+ */
+char *create_array(unsigned int size, char c)
+{
+	unsigned int i;
+	char *s;
+
+	if (size == 0)
+	return (NULL);
+	s = malloc(size * sizeof(char));
+	if (s == NULL)
+	for (i = 0; i < size; i++)
+	{
+	s[i] = c;
+	}
+	return (s);
+}
