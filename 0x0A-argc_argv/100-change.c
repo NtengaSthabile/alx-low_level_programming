0f0f0bb3e8073ab7a4dff7119354061c49d9e9d8
NtengaSthabile <ntengasthabile23@gmail.com>
2023-07-12 10:16:50 -0700

    Manyombose

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -13,39 +13,28 @@ int main(int argc, char *argv[])
 {
 	int cents, coins = 0;
 
-	if (argc != 2)
+	if (argc == 1 || argc > 2)
 	{
-	printf("Error\n");
-	return (1);
+		printf("Error\n");
+		return (1);
 	}
 	cents = atoi(argv[1]);
 	while (cents > 0)
 	{
-	coins++;
-	if ((cents - 25) >= 0)
+		if (cents >= 25)
+			cents -= 25;
+		else if (cents >= 10)
+			cents -= 10;
+		else if (cents >= 5)
+			cents -= 5;
+		else if (cents >= 2)
+			cents -= 2;
+		else if (cents >= 1)
+			cents -= 1;
+		coins += 1;
 	}
-	cents -= 25;
-	continue;
-	{
-	if ((cents - 10) >= 0)
-	{
-	cents -= 10;
-	continue;
-	}
-	if ((cents - 5) >= 0)
-	{
-	cents -= 5;
-	continue;
-	}
-	if ((cents - 2) >= 0)
-	{
-	cents -= 2;
-	continue;
-	}
-	cents--;
-	{
 	printf("%d\n", coins);
 	return (0);
-	}
-	}
 }
+
+
