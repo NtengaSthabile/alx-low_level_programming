cab68430a5aa3d8b29d527937743d1b533871d46
NtengaSthabile <ntengasthabile23@gmail.com>
2023-07-14 02:26:22 -0700

    damn

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -21,17 +21,17 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (s2 == NULL)
 		s2 = "";
 
-	for (ls1 = 0; s1[ls1]; = '\0', ls1++)
+	for (ls1 = 0; s1[ls1]!= '\0'; ls1++)
 		;
-	for (ls2 = 0; s2[ls2]; = '\0', ls2++)
+	for (ls2 = 0; s2[ls2]!= '\0'; ls2++)
 		;
 	if (n > ls2)
 		n = ls2;
 	lsout = ls1 + n;
-	sout = malloc(lsout; + 1);
-	if (lsout == NULL)
+	sout = malloc(lsout + 1);
+	if (sout == NULL)
 		return (NULL);
-	for (i = 0; < lsout; i++)
+	for (i = 0; lsout; i++)
 		if (i < ls1)
 			sout[i] = s1[i];
 		else
