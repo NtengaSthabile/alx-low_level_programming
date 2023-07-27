3fd0623e1c54fbdc5e3eee8b1106d608baf524d2
NtengaSthabile <ntengasthabile23@gmail.com>
2023-07-27 12:15:03 -0700

    Umzuzu naye uJesu

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -2,20 +2,23 @@
 #include "lists.h"
 
 /**
- * pointer to the list-t list to print
+ * print_list - prints all the elements of a linked list
+ * @h:pointer to the list-t list to print
  *
  * Return: the number of nodes printed
  */
 
 size_t print_list(const list_t *h)
 {
-	size -t s =0;
+	size_t s = 0;
 
 	while (h)
 	{
-		if (!h-> str)
-			printf (["%u] %s/n", h-> len, h-> str);
-		h=h -> next
+		if (!h->str)
+			printf("[0] (nill)(\n");
+		else
+			 printf("[%u] %s\n", h->len, h->str);
+		h = h->next;
 			s++;
 	}
 
