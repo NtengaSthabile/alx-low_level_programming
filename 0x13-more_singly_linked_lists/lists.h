a7a4de16282b0798a7bbd69ceceb3cf6e4e7efe0
NtengaSthabile <ntengasthabile23@gmail.com>
2023-08-01 12:47:37 -0700

    task 2

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -17,7 +17,7 @@ typedef struct listint_s
 {
 	int n;
 	struct listint_s *next;
-} listin_t;
+} listint_t;
 
 
 size_t print_listint(const listint_t *h);
@@ -34,7 +34,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index);
 listint_t *reverse_listint(listint_t **head);
 size_t print_listint_safe(const listint_t *head);
 size_t free_listint_safe(listint_t **h);
-listin_t *find_listin_loop(listin_t *head);
+listint_t *find_listint_loop(listint_t *head);
 
 #endif
 
